9deec517e818e5d803a6ea1a9e3a4e7f24073136
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-07-22 11:02:32 +0530

    Time: 45 ms (34.92%), Space: 13.5 MB (63.02%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.py
@@ -5,9 +5,8 @@ class Solution(object):
         :rtype: List[int]
         """
         d=0
-        g=[]
         for i in range(len(nums)):
             d+=nums[i]
-            g.append(d)
-        print(g)
-        return g
+            nums[i]=d
+        print(nums)
+        return nums
