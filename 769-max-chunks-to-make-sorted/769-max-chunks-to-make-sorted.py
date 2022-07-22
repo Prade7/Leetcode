7de4b62eec515f480ae56be6ecb2ae8d196569ef
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-07-22 13:40:01 +0530

    Time: 12 ms (97.87%), Space: 13.3 MB (56.38%) - LeetHub

--- 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.py
+++ 769-max-chunks-to-make-sorted/769-max-chunks-to-make-sorted.py
@@ -6,14 +6,11 @@ class Solution(object):
         """
         count=0
         a=sorted(arr)
-        # print(a)
         h=[]
         for ele in range(len(arr)):
             h.extend(sorted(arr[:ele]))
             h.extend(sorted(arr[ele::]))
             if(h==a):
-                print(h)
                 count+=1
             h=[]
-        print(count)
         return count
