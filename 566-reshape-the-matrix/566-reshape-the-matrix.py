c174508418bde377f06ce27fd5946083746039f3
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-07-22 20:06:12 +0530

    Time: 125 ms (30.01%), Space: 14 MB (74.29%) - LeetHub

---
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -0,0 +1,31 @@
+class Solution(object):
+    def matrixReshape(self, mat, r, c):
+        """
+        :type mat: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        
+        if(len(mat)*len(mat[0])!=(r*c)):
+            # print("jys")
+            return mat
+        f=0
+        res=[]
+        g=[]
+        for ele in mat:
+            for i in ele:
+                res.append(i)
+                f+=1
+                
+                if(f==c ):
+                    g.append(res)
+                    res=[]
+                    f=0
+        return g     
+        print(g)          
+        
+        
+        
+        
+        
