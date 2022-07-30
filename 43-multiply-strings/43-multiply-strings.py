73410c20f5816347d94eea453a99297af2a7c0be
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-07-30 19:41:11 +0530

    Time: 29 ms (72.44%), Space: 13.6 MB (24.60%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -5,10 +5,12 @@ class Solution(object):
         :type num2: str
         :rtype: str
         """
+        dict1={"0":48,"1":49,"2":50,"3":51,"4":52,"5":53,"6":54,"7":55,"8":56,"9":57}
         a=0
         for ele in num1:
-            a=a*10+(ord(ele)-48)
+            a=a*10+(dict1[ele]-48)
         b=0
         for ele in num2:
-            b=b*10+(ord(ele)-48)
+            b=b*10+(dict1[ele]-48)
         return str(a*b)
+        
