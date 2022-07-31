b90b85787ab49201391b6a8d7fb266300bd71d2b
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-07-31 12:52:20 +0530

    Time: 49 ms (100.00%), Space: 13.4 MB (100.00%) - LeetHub

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def minimumOperations(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if(nums[0]==0 and len(nums)==1):
+            return 0
+        nums.sort()
+        print(nums)
+        if(len(nums)==1):
+            return 1
+        a=0
+        nums=set(nums)
+        if(0 in nums):
+            nums.remove(0)
+        print(nums)
+        nums=list(nums)
+        if(nums==[]):
+            return 0
+        if(len(nums)==1):
+            return 1
+        count=0
+        while(True):
+            a=nums[0]
+            for i in range(0,len(nums)):
+                nums[i]-=a
+            count+=1
+            nums=set(nums)
+            nums=list(nums)
+            nums.remove(0)
+            if(len(nums)<=1):
+                count+=1
+                break
+            
+        return count
+            
