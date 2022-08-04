b05b81ae9e6c19c1efdea1e10651c4e417eb469b
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-04 14:23:58 +0530

    Time: 1094 ms (67.05%), Space: 22.9 MB (12.50%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        min_price=99999
+        profit=0
+        for ele in prices:
+            if(ele<min_price):
+                min_price=ele
+            if(ele-min_price>profit):
+                profit=ele-min_price
+        return profit
+        
+        
+#        
