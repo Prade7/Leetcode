9adf0925435f1424c01b51be98fb0b9d4bbfe720
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-05 10:15:12 +0530

    Time: 69 ms (25.77%), Space: 14.1 MB (5.55%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.py
+++ 88-merge-sorted-array/88-merge-sorted-array.py
@@ -4,12 +4,37 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         nums1[m:]=nums2
-        # for i in range(len(nums1)):
-        #     a=min(nums1[i:])
-        #     ind=nums1.index(a)
-        #     nums1[i],nums1[ind]=nums1[ind],nums1[i]
-        nums1.sort()
-        print(nums1)
+        
+        def sort(nums1):
+            if(len(nums1)>1):
+                left=nums1[:len(nums1)//2]
+                right=nums1[len(nums1)//2:]
+                i=0
+                j=0
+                k=0
+                sort(left)
+                sort(right)
+                while(i<len(left) and j<len(right)):
+                    if(left[i]<right[j]):
+                        nums1[k]=left[i]
+                        i+=1
+                    else:
+                        nums1[k]=right[j]
+                        j+=1
+                    k+=1
+                while(i<len(left)):
+                    nums1[k]=left[i]
+                    i+=1
+                    k+=1
+                while(j<len(right)):
+                    nums1[k]=right[j]
+                    j+=1
+                    k+=1
+                
+        return sort(nums1)
+        
+        
+    
         
         
         
