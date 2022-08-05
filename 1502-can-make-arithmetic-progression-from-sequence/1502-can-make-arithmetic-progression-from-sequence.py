988cbbeb247fb6f7ece02e3c754b761166fffe60
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-06 02:41:02 +0530

    Time: 58 ms (63.47%), Space: 14.1 MB (28.10%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.py
@@ -2,33 +2,34 @@ class Solution:
     def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
         
         
-        def merge(arr):
-            if(len(arr)>1):
-                left=arr[:len(arr)//2]
-                right=arr[len(arr)//2:]
-                merge(left)
-                merge(right)
+#         def merge(arr):
+#             if(len(arr)>1):
+#                 left=arr[:len(arr)//2]
+#                 right=arr[len(arr)//2:]
+#                 merge(left)
+#                 merge(right)
                 
-                i=0
-                j=0
-                k=0
-                while(i<len(left) and j<len(right)):
-                    if(left[i]<right[j]):
-                        arr[k]=left[i]
-                        i+=1
-                    else:
-                        arr[k]=right[j]
-                        j+=1
-                    k+=1
-                while(i<len(left)):
-                    arr[k]=left[i]
-                    i+=1
-                    k+=1
-                while(j<len(right)):
-                    arr[k]=right[j]
-                    j+=1
-                    k+=1
-        merge(arr)
+#                 i=0
+#                 j=0
+#                 k=0
+#                 while(i<len(left) and j<len(right)):
+#                     if(left[i]<right[j]):
+#                         arr[k]=left[i]
+#                         i+=1
+#                     else:
+#                         arr[k]=right[j]
+#                         j+=1
+#                     k+=1
+#                 while(i<len(left)):
+#                     arr[k]=left[i]
+#                     i+=1
+#                     k+=1
+#                 while(j<len(right)):
+#                     arr[k]=right[j]
+#                     j+=1
+#                     k+=1
+#         merge(arr)
+        arr.sort()
         diff=arr[1]-arr[0]
         for i in range(1,len(arr)-1):
             if(arr[i+1]-arr[i]!=diff):
@@ -37,4 +38,3 @@ class Solution:
             
         
         
-        print(arr)
