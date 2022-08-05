1f9b5b96a5e9c0b6c70662ccd7a4086fccc873a2
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-06 02:20:45 +0530

    Time: 532 ms (85.79%), Space: 31.2 MB (56.31%) - LeetHub

--- 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
+++ 1481-least-number-of-unique-integers-after-k-removals/1481-least-number-of-unique-integers-after-k-removals.py
@@ -6,17 +6,16 @@ class Solution:
                 dict1[ele]=1
             else:
                 dict1[ele]+=1
-        dict1=sorted(dict1.values())
-        # print(dict1)
-        sorted_values=dict1
+        sorted_values=sorted(dict1.values())
+        print(sorted_values)
         length=len(sorted_values)
         for ele in sorted_values:
             if(ele<=k):
-                print(ele)
+                print(k)
                 k-=ele
                 length-=1
-            else:
-                break
+            # else:
+            #     break
             
         return length
 
