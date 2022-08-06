50ccc9add1a954d9608c504b9ccb0cc3a6f76feb
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-06 23:18:30 +0530

    Time: 68 ms (22.61%), Space: 13.9 MB (59.14%) - LeetHub

--- 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.py
+++ 747-largest-number-at-least-twice-of-others/747-largest-number-at-least-twice-of-others.py
@@ -1,37 +1,13 @@
 class Solution:
     def dominantIndex(self, nums: List[int]) -> int:
-        q=nums[::]
-        def merge(nums):
-            if(len(nums)>1):
-                left=nums[:len(nums)//2]
-                right=nums[len(nums)//2:]
-
-                merge(left)
-                merge(right)
-                i=0
-                j=0
-                k=0
-                while(i<len(left) and j<len(right)):
-                    if(left[i]<right[j]):
-                        nums[k]=left[i]
-                        i+=1
-                    else:
-                        nums[k]=right[j]
-                        j+=1
-                    k+=1
-                while(i<len(left)):
-                    nums[k]=left[i]
-                    i+=1
-                    k+=1
-                while(j<len(right)):
-                    nums[k]=right[j]
-                    j+=1
-                    k+=1
-        merge(nums)
-        print(nums)
-        f=nums[-1]
-        
-        for ele in nums:
-            if(ele*2>f and ele!=f):
+        maxi=nums[0]
+        ind=0
+        for i,ele in enumerate(nums):
+            if(ele>maxi):
+                maxi=ele
+                ind=i
+        print(maxi)
+        for ele in range(len(nums)):
+            if(nums[ele]*2 > maxi and ele!=ind):
                 return -1
-        return q.index(f)
+        return ind
