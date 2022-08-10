64aa3b6163d26672ebf37c85e31e3cbbf9d9bc3e
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-10 18:14:59 +0530

    Time: 272 ms (54.19%), Space: 16.9 MB (82.41%) - LeetHub

--- 324-wiggle-sort-ii/324-wiggle-sort-ii.py
+++ 324-wiggle-sort-ii/324-wiggle-sort-ii.py
@@ -3,38 +3,48 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        def merge(nums):
-            if(len(nums)>1):
-                left=nums[:len(nums)//2]
-                right=nums[len(nums)//2:]
-                merge(left)
-                merge(right)
-                i=0
-                j=0
-                k=0
-                while(i<len(left) and j<len(right)):
-                    if(left[i]<right[j]):
-                        nums[k]=left[i]
-                        i+=1
-                    else:
-                        nums[k]=right[j]
-                        j+=1
-                    k+=1
-                while(i<len(left)):
-                    nums[k]=left[i]
-                    i+=1
-                    k+=1
-                while(j<len(right)):
-                    nums[k]=right[j]
-                    j+=1
-                    k+=1
-        merge(nums)
-        print(nums)
+#         def merge(nums):
+#             if(len(nums)>1):
+#                 left=nums[:len(nums)//2]
+#                 right=nums[len(nums)//2:]
+#                 merge(left)
+#                 merge(right)
+#                 i=0
+#                 j=0
+#                 k=0
+#                 while(i<len(left) and j<len(right)):
+#                     if(left[i]<right[j]):
+#                         nums[k]=left[i]
+#                         i+=1
+#                     else:
+#                         nums[k]=right[j]
+#                         j+=1
+#                     k+=1
+#                 while(i<len(left)):
+#                     nums[k]=left[i]
+#                     i+=1
+#                     k+=1
+#                 while(j<len(right)):
+#                     nums[k]=right[j]
+#                     j+=1
+#                     k+=1
+#         merge(nums)
+#         print(nums)
         
-        # nums[:len(nums)//2]=nums[::2]
-        nums[::]=nums[::-1]
-        nums[::2],nums[1::2]=nums[(len(nums)//2):],nums[:len(nums)//2]
+#         nums[::]=nums[::-1]
+#         nums[::2],nums[1::2]=nums[(len(nums)//2):],nums[:len(nums)//2]
+        # n=[]
+#         for ele in range(len(nums)//2):
+#             n.append(min(nums))
+#             nums.remove(min(nums))
+#             n.append(max(nums))
+#             nums.remove(max(nums))
+            
+            
+#         nums[::]=n[::]
+#         print(nums)
+
+        nums.sort(reverse=True)
+        nums[::2],nums[1::2]=nums[len(nums)//2:],nums[:len(nums)//2]
         
-    #      k = len(nums)//2
-    # nums.sort(reverse = True)
-    # nums[::2], nums[1::2] = nums[k:], nums[:k]
+    
