a7adb7d99ae90c596fbbb19ee158f6b465f268bd
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-13 02:00:22 +0530

    Time: 48 ms (65.77%), Space: 14 MB (14.36%) - LeetHub

---
+++ 202-happy-number/202-happy-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        while True:
+            sum = 0
+            while n != 0:
+                #if 19 / 10, remainder will be 9 and so on, it's how we separate digits 
+                rem = n % 10
+                sqr = rem*rem
+                sum = sum + sqr
+                #19 / 10 = 1.9, then int will round it to 1, it's how we get second digit
+                n = int(n/10)
+            #basically a task
+            if sum == 1:
+                return True
+            #if numbers are unhappy they will be in eternal cycle of 4, 16, 37, 58, 89, 145 and so on,
+            #idk why, if someone can explain I will appreciate it
+            elif sum == 4 or sum == 16:
+                return False
+            #to keep a cycle running
+            n = sum
