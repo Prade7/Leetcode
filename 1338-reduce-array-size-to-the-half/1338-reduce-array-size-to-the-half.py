9ecfd1c83cc06e9ebf01ced870273ed92b855059
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-18 18:04:20 +0530

    Time: 1015 ms (43.07%), Space: 31.8 MB (86.40%) - LeetHub

--- 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
+++ 1338-reduce-array-size-to-the-half/1338-reduce-array-size-to-the-half.py
@@ -1,9 +1,5 @@
 class Solution:
     def minSetSize(self, arr: List[int]) -> int:
-        
-        # arr.sort(key=lambda e:arr.count(e))
-        print(arr)
-        
         hm={}
         for e in arr:
             if(e not in hm):
@@ -12,23 +8,12 @@ class Solution:
                 hm[e]+=1
         
         values=list(hm.values())
-        print(values)
         count=0
         values.sort(reverse=True)
-        hald=len(arr)//2
-        while(hald>0):
-            hald-=values[count]
+        print(values)
+        l=len(arr)//2
+        while(l>0):
+            l-=values[count]
             count+=1
         return count
-        
-#         cnt = Counter(arr)      
-#         num_freq = sorted(cnt.values(), reverse=True) 
-#         print(num_freq)
-#         half_size = len(arr) // 2
-#         ans = 0
-        
-#         while half_size > 0:
-#             half_size -= num_freq[ans]
-#             ans += 1
-        
-#         return ans
+    
