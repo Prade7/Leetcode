def6449ab7cc9c65bfdb34954971ecaa9952f28a
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-20 15:45:21 +0530

    Time: 47 ms (49.44%), Space: 13.9 MB (24.55%) - LeetHub

---
+++ 290-word-pattern/290-word-pattern.py
@@ -0,0 +1,29 @@
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        s=s.split(" ")
+        if(len(s)!=len(pattern)):
+            return False
+      
+        if(len(set(s))==1 and len(set(pattern))==1):
+            return True
+        if(len(set(s))==1 and len(set(pattern))!=1):
+            return False
+        
+        if(len(set(pattern))!=len(set(s))):
+            return False
+        hm={}
+        for i in range(len(s)):
+            if(pattern[i] not in hm):
+                hm[pattern[i]]=s[i]
+        print(hm)
+        for i in range(len(pattern)):
+            if(hm[pattern[i]]!=s[i]):
+                print(hm[pattern[i]])
+                return False
+        
+        return True
+            
+        
+        
+        
+        
