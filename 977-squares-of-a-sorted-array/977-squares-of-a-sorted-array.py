7f1981443a0a80a73def05cb682fa198b6a85667
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-21 22:58:17 +0530

    Time: 367 ms (46.88%), Space: 16.3 MB (49.06%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.py
@@ -6,34 +6,35 @@ class Solution:
             result.append(ele*ele)
         print(result)
         
-        def merge(result):
-            if(len(result)>1):
-                left=result[:len(result)//2]
-                right=result[len(result)//2:]
-                merge(left)
-                merge(right)
-                i=0
-                j=0
-                k=0
-                while(i<len(left) and j<len(right)):
-                    if(left[i]<right[j]):
-                        result[k]=left[i]
-                        i+=1
-                        k+=1
-                    else:
-                        result[k]=right[j]
-                        j+=1
-                        k+=1
-                while(i<len(left)):
-                    result[k]=left[i]
-                    i+=1
-                    k+=1
-                while(j<len(right)):
-                    result[k]=right[j]
-                    j+=1
-                    k+=1
-        merge(result)
+        # def merge(result):
+        #     if(len(result)>1):
+        #         left=result[:len(result)//2]
+        #         right=result[len(result)//2:]
+        #         merge(left)
+        #         merge(right)
+        #         i=0
+        #         j=0
+        #         k=0
+        #         while(i<len(left) and j<len(right)):
+        #             if(left[i]<right[j]):
+        #                 result[k]=left[i]
+        #                 i+=1
+        #                 k+=1
+        #             else:
+        #                 result[k]=right[j]
+        #                 j+=1
+        #                 k+=1
+        #         while(i<len(left)):
+        #             result[k]=left[i]
+        #             i+=1
+        #             k+=1
+        #         while(j<len(right)):
+        #             result[k]=right[j]
+        #             j+=1
+        #             k+=1
+        # merge(result)
         
+        result.sort()
         return result
         
         
