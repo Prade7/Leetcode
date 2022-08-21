a7aab4ee7906569bceec5487ce64335b7e21dcd4
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-21 22:16:45 +0530

    Time: 133 ms (18.85%), Space: 15 MB (57.47%) - LeetHub

--- 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
+++ 345-reverse-vowels-of-a-string/345-reverse-vowels-of-a-string.py
@@ -13,7 +13,6 @@ class Solution:
                 end-=1
             elif(s[end] in vowels) and (s[start] not in vowels):
                 start+=1
-            # if(s[start] not in vowels) and (s[end] not in vowels):
             else:
                 start+=1
                 end-=1
@@ -21,23 +20,3 @@ class Solution:
         return "".join(s)
         
         
-        
-        #  d = ''
-        # start = 0
-        # end = len(s)-1
-        # s = list(s)
-        # l = ['a','e','i','o','u','A','E','I','O','U']
-        # while(start<end):
-        #     if s[start] in l and s[end] in l:
-        #         s[start],s[end] = s[end],s[start]  
-        #         start += 1
-        #         end -= 1
-        #     elif s[start] in l and s[end] not in l:
-        #         end -= 1
-        #     elif s[start] not in l and s[end] in l:
-        #         start += 1
-        #     else:
-        #         start += 1
-        #         end -= 1
-        # s = ''.join(s)
-        # return s
