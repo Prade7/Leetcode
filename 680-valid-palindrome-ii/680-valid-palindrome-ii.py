e08740864338ac4e99b7316594ae9673ac79edc2
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-22 11:10:55 +0530

    Time: 111 ms (92.58%), Space: 14.6 MB (47.02%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,36 +1,20 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
         
-#         if(s==s[::-1]):
-#             return True
-        
-#         start=0
-#         end=len(s)-1
-        
-#         while(start<end):
-            
-#             if(s[start]!=s[end]):
-#                 left=s[:start]+s[start+1:]
-#                 right=s[:end]+s[end+1:]
-                
-#                 if(left==left[::-1] or right[::]==right[::-1]):
-#                     return True
-#             start+=1
-#             end-=1
-            
-#         return False
-    
-     # def validPalindrome(self, s: str) -> bool:
-        if s == s[::-1]:
+        if(s==s[::-1]):
             return True
         
-        left = 0
-        right = len(s)-1
-        while left < right:
-            if s[left] != s[right]:
-                leftstring = s[:left]+s[left+1:]
-                rightstring = s[:right]+s[right+1:]
-                return leftstring == leftstring[::-1] or rightstring == rightstring[::-1]
-            left += 1
-            right -= 1
-        return True
+        start=0
+        end=len(s)-1
+        
+        while(start<end):
+            
+            if(s[start]!=s[end]):
+                left=s[:start]+s[start+1:]
+                right=s[:end]+s[end+1:]
+                
+                return (left==left[::-1] or right[::]==right[::-1])
+            start+=1
+            end-=1
+            
+        return False
