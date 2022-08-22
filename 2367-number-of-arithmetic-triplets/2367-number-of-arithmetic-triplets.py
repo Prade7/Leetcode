04fa6f38d0179f7cc69aae777a900b62ea391dec
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-22 12:54:41 +0530

    Time: 1407 ms (25.51%), Space: 13.9 MB (19.95%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -5,7 +5,19 @@ class Solution:
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 for k in range(j+1,len(nums)):
-                    # if(i!=j and j!=k):
                     if(nums[j]-nums[i]==diff and nums[k]-nums[j]==diff):
                         count+=1
         return (count)
+    
+        i=0
+        j=len(nums)-1
+        count=0
+        while(i<j):
+            k=i+1
+            while(k<j):
+                if(nums[k]-nums[i]==diff and nums[j]-nums[k]==diff):
+                    count+=1
+                k+=1
+            i+=1
+            j-=1
+        print(count)
