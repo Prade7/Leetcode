1801f08f6944c12e7908f9f90d380da29dd72594
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-08-29 10:04:10 +0530

    Time: 260 ms (5.06%), Space: 14.2 MB (26.08%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.py
@@ -1,12 +1,41 @@
-class Solution(object):
-    def findMedianSortedArrays(self, nums1, nums2):
-        nums1.extend(nums2)
-        # nums=nums1
-        nums1.sort()
-        n=len(nums1)//2
-
-        if(len(nums1)%2==0):
-            return (float(nums1[n]+nums1[n-1]))/2
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        
+        nums1[len(nums1):]=nums2[::]
+        
+        def merge(nums1):
+            if(len(nums1)>1):
+                left=nums1[len(nums1)//2:]
+                right=nums1[:len(nums1)//2]
+                merge(left)
+                merge(right)
+                i=0
+                j=0
+                k=0
+                
+                while(i<len(left) and j<len(right)):
+                    if(left[i]<right[j]):
+                        nums1[k]=left[i]
+                        i+=1
+                        k+=1
+                    else:
+                        nums1[k]=right[j]
+                        j+=1
+                        k+=1
+                        
+                while(i<len(left)):
+                    nums1[k]=left[i]
+                    i+=1
+                    k+=1
+                while(j<len(right)):
+                    nums1[k]=right[j]
+                    j+=1
+                    k+=1
+        merge(nums1)
+       
+        print(nums1)
+        if(len(nums1)%2!=0):
+            return float(nums1[(len(nums1)//2)])
         else:
-            return float(nums1[n])
-            
+            return (nums1[(len(nums1)//2)-1]+nums1[len(nums1)//2])/2
+        
