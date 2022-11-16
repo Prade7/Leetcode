2e00b06a60b5a3492299375d5cf66d0f5d715d47
Prade7 <87432021+Prade7@users.noreply.github.com>
2022-11-16 20:35:01 +0530

    Time: 62 ms (25.19%), Space: 13.9 MB (9.07%) - LeetHub

--- 1323-maximum-69-number/1323-maximum-69-number.py
+++ 1323-maximum-69-number/1323-maximum-69-number.py
@@ -19,6 +19,37 @@ class Solution:
             if(len(result_list)==len(str_num)):
                 break
         print(result_list)
-        return max(result_list)
+        
+        def merge_sort(result_list):
+            if(len(result_list)>1):
+                left=result_list[:len(result_list)//2]
+                right=result_list[len(result_list)//2:]
+                merge_sort(left)
+                merge_sort(right)
+                
+                i=0
+                j=0
+                k=0
+                while(i<len(left) and j<len(right)):
+                    if(left[i]<right[j]):
+                        result_list[k]=left[i]
+                        i+=1
+                    else:
+                        result_list[k]=right[j]
+                        j+=1
+                    k+=1
+                while(i<len(left)):
+                    result_list[k]=left[i]
+                    i+=1
+                    k+=1
+                while(j<len(right)):
+                    result_list[k]=right[j]
+                    j+=1
+                    k+=1
+            return result_list[-1]
+        return merge_sort(result_list)
+                
+               
+        # return max(result_list)
         
            
